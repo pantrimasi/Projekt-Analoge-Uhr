efe525ab447fe71256b8c95d21fcbdafc244bbd2
Masato Kuster <pantrimasi@gmail.com>
2025-11-17 16:06:11 +0100

    3

--- analogClock.js
+++ analogClock.js
@@ -9,14 +9,15 @@ function updateClock() {
   const now = new Date();
 
   const sec = now.getSeconds();
-  const secDeg = (sec / 60) * 360;
+  const secDeg = (sec / 60) * 360 - 90;
 
   const min = now.getMinutes();
-  const minDeg = (min / 60) * 360 + (sec / 60) * 6;
+  const minDeg = (min / 60) * 360 + (sec / 60) * 6 - 90;
 
   const hour = now.getHours();
-  const hourDeg = (hour % 12) * 30 + (min / 60) * 30;
-
+  const hourDeg =
+    (hour % 12) * 30 + (min / 60) * 30 - 90;
+    
   secHand.style.transform = `rotate(${secDeg}deg)`;
   minHand.style.transform = `rotate(${minDeg}deg)`;
   hourHand.style.transform = `rotate(${hourDeg}deg)`;
