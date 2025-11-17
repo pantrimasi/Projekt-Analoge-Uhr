5ec159372e06269845a8d98517c894490526f387
Masato Kuster <pantrimasi@gmail.com>
2025-11-17 16:21:33 +0100

    Final

--- analogClock.js
+++ analogClock.js
@@ -1,23 +1,30 @@
 // Author: Masato Kuster
 // Date: 17.11.2025
 
+/* Get Clock Elements*/
 const hourHand = document.querySelector(".hour-hand");
 const minHand = document.querySelector(".min-hand");
 const secHand = document.querySelector(".second-hand");
 
+
+/* Realtime Rotation */
 function updateClock() {
   const now = new Date();
 
+  // seconds
   const sec = now.getSeconds();
   const secDeg = (sec / 60) * 360 - 90;
 
+  // minutes
   const min = now.getMinutes();
   const minDeg = (min / 60) * 360 + (sec / 60) * 6 - 90;
 
+  // hours
   const hour = now.getHours();
   const hourDeg =
     (hour % 12) * 30 + (min / 60) * 30 - 90;
-    
+
+  // apply rotation
   secHand.style.transform = `rotate(${secDeg}deg)`;
   minHand.style.transform = `rotate(${minDeg}deg)`;
   hourHand.style.transform = `rotate(${hourDeg}deg)`;
